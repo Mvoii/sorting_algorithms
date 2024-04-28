4c3e7b40c7f891f4ac8ffe886b4b429c41cddbbf
Franklin Mvoi <mvoifranklin@gmail.com>
2024-04-28 18:37:00 +0300

    checker fail #text fail and requirement 4

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,7 @@ void bubble_sort(int *array, size_t size)
 {
     int tmp;
     size_t i, j;
+    int swapped;
 
     if (array == NULL || size < 2)
         return;
@@ -17,6 +18,8 @@ void bubble_sort(int *array, size_t size)
     /* pass through the array */
     for (i = 0; i < size - 1; i++)
     {
+        swapped = 0;
+
         for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
@@ -26,9 +29,13 @@ void bubble_sort(int *array, size_t size)
                 array[j] = array[j + 1];
                 array[j + 1] = tmp;
 
+                swapped = 1;
+
                 /* print after each swap */
                 print_array(array, size);
             }
         }
+        if (swapped == 0)
+            return;
     }
 }
