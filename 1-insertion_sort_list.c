773d0856e5224c96ac6727d72d90ecbe4a247db9
Franklin Mvoi <mvoifranklin@gmail.com>
2024-04-29 15:07:53 +0300

    task: 1-0

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * @list: doubly linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
